bd22dc071c8f794f115f115efc2c595e3f183074
toshiki kuwa <toshiki kuwa>
2016-04-28 19:15:57 +0900

    add proc

---
+++ HogeProcBase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * base main routine
+ */
+class HogeBaseProc {
+
+    /**
+     * 成功結果
+     *  0:fail, 1:success
+     * @var int
+     */
+    public $success = 0;
+
+    /**
+     * エラーメッセージ
+     * @var string
+     */
+    public $msg = "";
+
+}
